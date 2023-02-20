f2d89e0620add20f52444a8ba8da818bfa5f018f
Eathyn <eathynzakas@gmail.com>
2023-02-20 17:27:12 +0800

    viewport scale

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 import './css/index.css'
+
+const DPR = Math.round(window.devicePixelRatio)
+const scale = 1 / DPR
+const meta = document.querySelector('meta[name="viewport"]')
+if (!meta) {
+  const meta = document.createElement('meta')
+  meta.setAttribute('name', 'viewport')
+}
+meta.setAttribute('content', `
+  width=device-width,
+  initial-scale=${scale},
+  minimum-scale=${scale},
+  user-scalable=no
+`)
